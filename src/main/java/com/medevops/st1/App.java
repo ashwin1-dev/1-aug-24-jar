f5d06a2eb445d71825b8008d7a2e2393ea46d4be
root <root@st1.server1.edu>
2024-08-01 09:54:19 +0000

    jar file

---
+++ src/main/java/com/medevops/st1/App.java
@@ -0,0 +1,15 @@
+package com.medevops.st1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello india" );
+        System.out.println( "Hello maharashtra" );
+    }
+}
